850380caf403e47977c4c2fb6568fc5effdb46ea
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-03-29 23:26:17 +0530

    viteconfing added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://ideavault-mern-full-stack-notes-app.onrender.com",
+        target: "http://localhost:3000",
         secure: false,
-        changeOrigin: true, // Add this line
       },
     },
   },
