36fd5ce21f3c7300d2960b448d46cae2ff09a9d2
SarangaSiriwardhana9 <lasindusaranga99@gmail.com>
2024-03-29 20:31:51 +0530

    404 page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Profile from "./pages/Profile";
-import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
-import CreateNote from "./pages/CreateNote";
-import MyNotes from "./pages/MyNotes";
-import UpdateNote from "./pages/UpdateNote";
-import Footer from "./components/Footer";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import CreateNote from './pages/CreateNote';
+import MyNotes from './pages/MyNotes';
+import UpdateNote from './pages/UpdateNote';
+import Footer from './components/Footer';
+import NotFound from './pages/NotFound'; // Import the NotFound component
 
 export default function App() {
   return (
@@ -29,9 +29,9 @@ export default function App() {
             <Route path='/mynotes' element={<MyNotes />} />
             <Route path='/update-note/:id' element={<UpdateNote />} />
           </Route>
+          <Route path='*' element={<NotFound />} /> {/* Route for the 404 error page */}
         </Routes>
       </BrowserRouter>
-      <ToastContainer />
       <Footer />
     </>
   );
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
+      <p className="text-xl text-gray-600 mt-4">The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
